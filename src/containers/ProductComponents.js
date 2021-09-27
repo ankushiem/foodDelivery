26de5660cbef726cf8332c9da2876f77b9c70053
ankushiem <84512750+ankushiem@users.noreply.github.com>
2021-09-28 03:34:12 +0530

    Update ProductComponents.js

--- src/containers/ProductComponents.js
+++ src/containers/ProductComponents.js
@@ -13,11 +13,12 @@ const ProductComponent = () => {
     //     )
     // }
     const renderList = products.map((product) => {
-        const {id, avgRating,cuisines ,deliveryTime,name,costForTwoString} = product;
+        const {id, avgRating,cuisines ,deliveryTime,name,costForTwoString,cloudinaryImageId} = product;
         return (
             <div key={id}>
             <Link to = {`/food-power.glitch.me/restaurant/${id}`} >
             <div className="container">
+            <img src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`} alt="food"/>
                 {/* {console.log(id)} */}
                 <h2> {name}</h2>
                 <h3>{cuisines}  </h3>
