2a3c82036d9a619d87e1a8f257ec14757aa01607
ankushiem <84512750+ankushiem@users.noreply.github.com>
2021-09-28 03:31:11 +0530

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { productReducer,selectedProductReducer } from "./productReducer";
+import changeTheNumber from "./upDown";
 
  const reducers = combineReducers({
     allProducts: productReducer,
     product: selectedProductReducer,
+    changeTheNumber,
 });
 
 export default reducers;
